271c9d9d40439ae7312c961bf1ef670da7a4449d
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-09-01 17:36:40 +0800

    Setup basic test suites to verify the routes and project handler methods

--- tests/2_functional-tests.js
+++ tests/2_functional-tests.js
@@ -16,93 +16,127 @@ chai.use(chaiHttp);
 suite('Functional Tests', function() {
   
     suite('POST /api/issues/{project} => object with issue data', function() {
-      
-      test('Every field filled in', function(done) {
-       chai.request(server)
-        .post('/api/issues/test')
-        .send({
-          issue_title: 'Title',
-          issue_text: 'text',
-          created_by: 'Functional Test - Every field filled in',
-          assigned_to: 'Chai and Mocha',
-          status_text: 'In QA'
-        })
-        .end(function(err, res){
-          assert.equal(res.status, 200);
+      test('POST: Create project msg', (done) => {
+        chai.request(server)
+          .post('/api/issues/')
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.equal(res.body, 'Create project')
+            done()
+          })
+      })
+      // test('Every field filled in', function(done) {
+      //  chai.request(server)
+      //   .post('/api/issues/test')
+      //   .send({
+      //     issue_title: 'Title',
+      //     issue_text: 'text',
+      //     created_by: 'Functional Test - Every field filled in',
+      //     assigned_to: 'Chai and Mocha',
+      //     status_text: 'In QA'
+      //   })
+      //   .end(function(err, res){
+      //     assert.equal(res.status, 200);
           
-          //fill me in too!
+      //     //fill me in too!
           
-          done();
-        });
-      });
+      //     done();
+      //   });
+      //});
       
-      test('Required fields filled in', function(done) {
+      // test('Required fields filled in', function(done) {
         
-      });
+      // });
       
-      test('Missing required fields', function(done) {
+      // test('Missing required fields', function(done) {
         
-      });
+      // });
       
     });
     
     suite('PUT /api/issues/{project} => text', function() {
-      
-      test('No body', function(done) {
+
+      test('PUT: Update project msg', (done) => {
+        chai.request(server)
+          .put('/api/issues/')
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.equal(res.body, 'Update project')
+            done()
+          })
+      })
+      // test('No body', function(done) {
         
-      });
+      // });
       
-      test('One field to update', function(done) {
+      // test('One field to update', function(done) {
         
-      });
+      // });
       
-      test('Multiple fields to update', function(done) {
+      // test('Multiple fields to update', function(done) {
         
-      });
+      // });
       
     });
     
     suite('GET /api/issues/{project} => Array of objects with issue data', function() {
-      
-      test('No filter', function(done) {
+
+      test('GET: Display project msg', (done) => {
         chai.request(server)
-        .get('/api/issues/test')
-        .query({})
-        .end(function(err, res){
-          assert.equal(res.status, 200);
-          assert.isArray(res.body);
-          assert.property(res.body[0], 'issue_title');
-          assert.property(res.body[0], 'issue_text');
-          assert.property(res.body[0], 'created_on');
-          assert.property(res.body[0], 'updated_on');
-          assert.property(res.body[0], 'created_by');
-          assert.property(res.body[0], 'assigned_to');
-          assert.property(res.body[0], 'open');
-          assert.property(res.body[0], 'status_text');
-          assert.property(res.body[0], '_id');
-          done();
-        });
-      });
+          .get('/api/issues/')
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.equal(res.body, 'Display project')
+            done()
+          })
+      }) 
+      // test('No filter', function(done) {
+      //   chai.request(server)
+      //   .get('/api/issues/test')
+      //   .query({})
+      //   .end(function(err, res){
+      //     assert.equal(res.status, 200);
+      //     assert.isArray(res.body);
+      //     assert.property(res.body[0], 'issue_title');
+      //     assert.property(res.body[0], 'issue_text');
+      //     assert.property(res.body[0], 'created_on');
+      //     assert.property(res.body[0], 'updated_on');
+      //     assert.property(res.body[0], 'created_by');
+      //     assert.property(res.body[0], 'assigned_to');
+      //     assert.property(res.body[0], 'open');
+      //     assert.property(res.body[0], 'status_text');
+      //     assert.property(res.body[0], '_id');
+      //     done();
+      //   });
+      // });
       
-      test('One filter', function(done) {
+      // test('One filter', function(done) {
         
-      });
+      // });
       
-      test('Multiple filters (test for multiple fields you know will be in the db for a return)', function(done) {
+      // test('Multiple filters (test for multiple fields you know will be in the db for a return)', function(done) {
         
-      });
+      // });
       
     });
     
     suite('DELETE /api/issues/{project} => text', function() {
-      
-      test('No _id', function(done) {
+      test('DELETE: Delete project msg', (done) => {
+        chai.request(server)
+          .delete('/api/issues/')
+          .end((err, res) => {
+            assert.equal(res.status, 200)
+            assert.equal(res.body, 'Delete project')
+            done()
+          })
+      })
+      // test('No _id', function(done) {
         
-      });
+      // });
       
-      test('Valid _id', function(done) {
+      // test('Valid _id', function(done) {
         
-      });
+      // });
       
     });
 
