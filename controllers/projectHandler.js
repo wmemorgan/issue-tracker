cea1e15e5b6d9940a353e0ff6ea685e27c6471ab
Wilfred Morgan <wil.me.morgan@gmail.com>
2018-09-02 03:11:58 +0800

    Identify required fields in projectCreate function

--- controllers/projectHandler.js
+++ controllers/projectHandler.js
@@ -2,6 +2,11 @@ const mongo = require('mongodb').MongoClient;
 const ObjectId = require('mongodb').ObjectID;
 
 const CONNECTION_STRING = process.env.DATABASE; //MongoClient.connect(CONNECTION_STRING, function(err, db) {});
+const requiredFields = [
+  'issue_title',
+  'issue_text',
+  'created_by'
+]
 let db
 
 mongo.connect(CONNECTION_STRING, function (err, conn) {
@@ -14,9 +19,17 @@ mongo.connect(CONNECTION_STRING, function (err, conn) {
 
 // Create project
 exports.projectCreate = (req, res) => {
- let project = req.body
-  console.log(`Project contains: `, project)
-  res.json(project)
+  let project = req.body
+  var missingFields = requiredFields.filter(field => !project.hasOwnProperty(field))
+  console.log(missingFields)
+  if (missingFields.length > 0) {
+    res.status(500)
+    res.send(`Missing fields: ${missingFields}`)
+  } else {
+    console.log(`Project contains: `, project)
+    res.json(project)
+  }
+
 }
 
 // Update project
